52584dca18650369950f2cc29d0749560ffcf628
rentalname <agenda21st@gmail.com>
2013-03-23 16:27:35 +0900

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAjax::Application.config.secret_token = '16f3a288c14397b24e82ee83cf60b6a98a46bc6e306b9288372a1e15948f59496aab5149ff3f71c4fc4e48165ab63a25365d26a44a7ddcbc4f60bf87e9848510'
